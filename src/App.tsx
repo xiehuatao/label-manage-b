a1e327661977d078bc3872e4dd52c891f602ed68
wangfanc <3399384334@qq.com>
2022-09-19 11:38:41 +0800

    wf-

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Switch} from "react-router-dom";
-
 import 'antd/dist/antd.min.css';
-
 import Management from "./components/information_management_function_page";
+import ManagementPage from "./components/information_management_page";
 
 
 
@@ -14,8 +13,9 @@ function App() {
       <Switch>
         {/*<Route path='/' component={Login} />*/}
         {/*  <Classify/>*/}
-          <Management/>
+        {/*  <Management/>*/}
         {/*  <Classification/>*/}
+          <ManagementPage/>
       </Switch>
     </BrowserRouter>
   );
--- src/components/classification/public_module/Red_star/Star.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Star.css"
-const Star = ()=>{
-    return <div className="Star">*</div>
-}
-export default Star;
-
